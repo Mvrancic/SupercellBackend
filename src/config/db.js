f959ec0a57143a079c74c97670034074feb099ac
Mvrancic <mvrancic@mail.austral.edu.ar>
2024-06-03 11:06:42 -0300

    .

--- src/config/db.js
+++ src/config/db.js
@@ -8,6 +8,10 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      ssl: true,
+      sslValidate: false,
+      useCreateIndex: true,
+      useFindAndModify: false,
     });
     console.log('Conectado a MongoDB');
   } catch (err) {
