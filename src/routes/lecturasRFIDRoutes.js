a2568df821c4b0d1546a51fd568d6667cef770c5
Mvrancic <mvrancic@mail.austral.edu.ar>
2024-06-05 22:10:15 -0300

    .

--- src/routes/lecturasRFIDRoutes.js
+++ src/routes/lecturasRFIDRoutes.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const lecturasRFIDController = require('../controllers/lecturasRFIDController');
 
-// Ruta para agregar una nueva tarjeta RFID
-router.post('/agregar_tarjeta', lecturasRFIDController.agregarTarjeta);
-
-// Ruta para obtener el último ID de tarjeta RFID registrado
-router.get('/ultimo_id_tarjeta', lecturasRFIDController.getUltimoIdTarjeta);
+router.get('/ultimoId', lecturasRFIDController.getUltimoIdTarjeta);
 
 module.exports = router;
